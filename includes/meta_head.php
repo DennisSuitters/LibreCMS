78fe8e200e047fd3aa11b56450df8abd95eedabc
StudioJunkyard <info@studiojunkyard.com>
2014-12-05 18:26:26 +1100

    Code Fix/Reduce

--- includes/meta_head.php
+++ includes/meta_head.php
@@ -13,20 +13,75 @@ $head.='<link rel="apple-touch-icon" href="'.PROTOCOL.$_SERVER['HTTP_HOST'].$con
 $head.='<meta name="viewport" content="width=400,initial-scale=1.0" />';
 $head.='<link rel="stylesheet" type="text/css" href="'.THEME.'/css/bootstrap.min.css" />';
 $head.='<link rel="stylesheet" type="text/css" href="includes/css/font-awesome.min.css" />';
-$head.='<link rel="stylesheet" type="text/css" href="includes/css/admin.css" />';
 if($view=='contactus')
 	$head.='<link rel="stylesheet" type="text/css" href="includes/css/mapsed.css" />';
-    $head.='<link rel="stylesheet" type="text/css" href="'.THEME.'/css/style.css" />';
+$head.='<link rel="stylesheet" type="text/css" href="'.THEME.'/css/style.css" />';
 if($user['rank']>699){
 	if($view=='bookings'||$view=='events'){
 		$head.='<link rel="stylesheet" type="text/css" href="includes/css/fullcalendar.min.css" />';
-        $head.='<link rel="stylesheet" type="text/css" href="includes/css/bootstrap-datetimepicker.min.css" />';
-    }
-    $head.='<link rel="stylesheet" type="text/css" href="includes/css/summernote.css" />';
+		$head.='<link rel="stylesheet" type="text/css" href="includes/css/bootstrap-datetimepicker.min.css" />';
+	}
+	$head.='<link rel="stylesheet" type="text/css" href="includes/css/summernote.css" />';
 }
+$head.='<link rel="stylesheet" type="text/css" href="includes/css/admin.css" />';
 $head.='</head><body><div id="top" class="hidden"></div>';
 if($user['rank']>699){
-    if($view=='index'||$view=='article'||$view=='portfolio'||$view=='bookings'||$view=='events'||$view=='news'||$view=='testimonials'||$view=='inventory'||$view=='services'||$view=='gallery'||$view=='contactus'||$view=='tos'||$view=='search'){
-	$head.='<div id="seohead"><div id="seocontent"><div class="panel-body"><div class="form-group"><label for="seoTitle" class="control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Title</label><div class="input-group col-lg-10 col-md-9 col-sm-9 col-xs-7"><input type="text" id="seoTitle" class="form-control" value="'.$page['seoTitle'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoTitle" placeholder="Enter a Page Title..."><div id="seoTitlesave" class="input-group-btn hidden"><button class="btn btn-danger"><i class="fa fa-save"></i></button></div></div></div><div class="form-group"><label for="seoCaption" class="control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Caption</label><div class="input-group col-lg-10 col-md-9 col-sm-9 col-xs-7"><input type="text" id="seoCaption" class="form-control" value="'.$page['seoCaption'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoCaption" placeholder="Enter a Page Caption..."><div id="seoCaptionsave" class="input-group-btn hidden"><button class="btn btn-danger"><i class="fa fa-save"></i></button></div></div></div><div class="form-group"><label for="seoDescription" class="control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Description</label><div class="input-group col-lg-10 col-md-9 col-sm-9 col-xs-7"><input type="text" id="seoDescription" class="form-control" value="'.$page['seoDescription'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoDescription" placeholder="Enter a Page Description..."><div id="seoDescriptionsave" class="input-group-btn hidden"><button class="btn btn-danger"><i class="fa fa-save"></i></button></div></div></div><div class="form-group"><label for="seoKeywords" class="control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Keywords</label><div class="input-group col-lg-10 col-md-9 col-sm-9 col-xs-7"><input type="text" id="seoKeywords" class="form-control" value="'.$page['seoKeywords'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoKeywords" placeholder="Enter Page Keywords..."><div id="seoKeywordssave" class="input-group-btn hidden"><button class="btn btn-danger"><i class="fa fa-save"></i></button></div></div></div><div class="form-group"><label for="notes" class="control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Notes</label><div class="input-group col-lg-10 col-md-9 col-sm-9 col-xs-7"><form method="post" target="sp" action="includes/update.php"><input type="hidden" name="id" value="'.$page['id'].'"><input type="hidden" name="t" value="menu"><input type="hidden" name="c" value="notes"><textarea id="notes" class="form-control summernote" name="da">'.$page['notes'].'</textarea></form></div></div></div></div><div id="seodrop"><a href=""><i class="fa fa-angle-double-down"></i></a></div></div>';
+	if($view=='index'||$view=='article'||$view=='portfolio'||$view=='bookings'||$view=='events'||$view=='news'||$view=='testimonials'||$view=='inventory'||$view=='services'||$view=='gallery'||$view=='contactus'||$view=='tos'||$view=='search'){
+	$head.='<div id="seohead">';
+		$head.='<div id="seocontent">';
+			$head.='<div class="libr8 panel-body">';
+				$head.='<div class="libr8 form-group">';
+					$head.='<label for="seoTitle" class="libr8 control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Title</label>';
+					$head.='<div class="libr8 input-group col-lg-10 col-md-9 col-sm-9 col-xs-7">';
+						$head.='<input type="text" id="seoTitle" class="libr8 form-control" value="'.$page['seoTitle'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoTitle" placeholder="Enter a Page Title...">';
+						$head.='<div id="seoTitlesave" class="input-group-btn hidden">';
+							$head.='<button class="libr8 btn btn-danger"><i class="fa fa-save"></i></button>';
+						$head.='</div>';
+					$head.='</div>';
+				$head.='</div>';
+				$head.='<div class="libr8 form-group">';
+					$head.='<label for="seoCaption" class="libr8 control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Caption</label>';
+					$head.='<div class="libr8 input-group col-lg-10 col-md-9 col-sm-9 col-xs-7">';
+						$head.='<input type="text" id="seoCaption" class="libr8 form-control" value="'.$page['seoCaption'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoCaption" placeholder="Enter a Page Caption...">';
+						$head.='<div id="seoCaptionsave" class="libr8 input-group-btn hidden">';
+							$head.='<button class="libr8 btn btn-danger"><i class="fa fa-save"></i></button>';
+						$head.='</div>';
+					$head.='</div>';
+				$head.='</div>';
+				$head.='<div class="libr8 form-group">';
+					$head.='<label for="seoDescription" class="libr8 control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Description</label>';
+					$head.='<div class="libr8 input-group col-lg-10 col-md-9 col-sm-9 col-xs-7">';
+						$head.='<input type="text" id="seoDescription" class="libr8 form-control" value="'.$page['seoDescription'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoDescription" placeholder="Enter a Page Description...">';
+						$head.='<div id="seoDescriptionsave" class="libr8 input-group-btn hidden">';
+							$head.='<button class="libr8 btn btn-danger"><i class="fa fa-save"></i></button>';
+						$head.='</div>';
+					$head.='</div>';
+				$head.='</div>';
+				$head.='<div class="libr8 form-group">';
+					$head.='<label for="seoKeywords" class="libr8 control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Keywords</label>';
+					$head.='<div class="libr8 input-group col-lg-10 col-md-9 col-sm-9 col-xs-7">';
+						$head.='<input type="text" id="seoKeywords" class="libr8 form-control" value="'.$page['seoKeywords'].'" data-dbid="'.$page['id'].'" data-dbt="menu" data-dbc="seoKeywords" placeholder="Enter Page Keywords...">';
+						$head.='<div id="seoKeywordssave" class="libr8 input-group-btn hidden">';
+							$head.='<button class="libr8 btn btn-danger"><i class="fa fa-save"></i></button>';
+						$head.='</div>';
+					$head.='</div>';
+				$head.='</div>';
+				$head.='<div class="libr8 form-group">';
+					$head.='<label for="notes" class="libr8 control-label col-lg-2 col-md-3 col-sm-3 col-xs-5">Page Notes</label>';
+					$head.='<div class="libr8 input-group col-lg-10 col-md-9 col-sm-9 col-xs-7">';
+						$head.='<form method="post" target="sp" action="includes/update.php">';
+							$head.='<input type="hidden" name="id" value="'.$page['id'].'">';
+							$head.='<input type="hidden" name="t" value="menu">';
+							$head.='<input type="hidden" name="c" value="notes">';
+							$head.='<textarea id="notes" class="libr8 form-control summernote" name="da">'.$page['notes'].'</textarea>';
+						$head.='</form>';
+					$head.='</div>';
+				$head.='</div>';
+			$head.='</div>';
+		$head.='</div>';
+		$head.='<div id="seodrop">';
+			$head.='<a href=""><i class="fa fa-angle-double-down"></i></a>';
+		$head.='</div>';
+	$head.='</div>';
 	}
 }
--- view/account.php
+++ view/account.php
@@ -1,40 +1,63 @@
 <?php
 if($user['rank']>0){
-    $content.='<div id="libr8_admin_header" class="nopadding nomargin"><nav class="navbar navbar-inverse no-margin" role="navigation"><div class="container-fluid"><div class="navbar-header"><button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#libr8-admin-collapse"><span class="sr-only">Administration</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>';
+    $content.='<div id="libr8_admin_header" class="libr8 nopadding nomargin">';
+		$content.='<nav class="libr8 navbar navbar-inverse no-margin" role="navigation">';
+			$content.='<div class="libr8 container-fluid">';
+				$content.='<div class="libr8 navbar-header">';
+					$content.='<button type="button" class="libr8 navbar-toggle" data-toggle="collapse" data-target="#libr8-admin-collapse">';
+						$content.='<span class="libr8 sr-only">Administration</span>';
+						$content.='<span class="libr8 icon-bar"></span>';
+						$content.='<span class="libr8 icon-bar"></span>';
+						$content.='<span class="libr8 icon-bar"></span>';
+					$content.='</button>';
     if($user['rank']>699){
-        $content.='<span class="navbar-brand"><a class="admin_logo" href=""><img src="images/libr8.png"></a></span>';
+        $content.='<span class="libr8 navbar-brand"><a class="libr8 admin_logo" href=""><img src="images/libr8.png"></a></span>';
     }
-	$content.='</div><div class="collapse navbar-collapse" id="libr8-admin-collapse"><ul class="nav navbar-nav pull-right relative" style="margin-right:20px;">';
+	$content.='</div>';
+	$content.='<div class="libr8 collapse navbar-collapse" id="libr8-admin-collapse">';
+		$content.='<ul class="libr8 nav navbar-nav pull-right relative" style="margin-right:20px;">';
     if($user['rank']>899){
         if($user['options']{3}==1){
-           $content.='<li';if($view=='orders'){$content.=' class="active"';}$content.='><a href="admin/orders/all">Orders</a></li>';
+			$content.='<li';if($view=='orders'){$content.=' class="libr8 active"';}$content.='><a href="admin/orders/all">Orders</a></li>';
         }
-       $content.='<li';if($view=='media'){$content.=' class="active"';}$content.='><a href="admin/media">Media</a></li>';
+       $content.='<li';if($view=='media'){$content.=' class="libr8 active"';}$content.='><a href="admin/media">Media</a></li>';
         if($user['options']{5}==1){
-            $content.='<li';if($view=='accounts'){$content.=' class="active"';}$content.='><a href="admin/accounts">Accounts</a></li>';
+			$content.='<li';if($view=='accounts'){$content.=' class="libr8 active"';}$content.='><a href="admin/accounts">Accounts</a></li>';
         }
 	   if($user['options']{0}==1){
-           $content.='<li';if($view=='proofs'){$content.=' class="active"';}$content.='><a href="admin/proofs">Proofs</a></li>';
+			$content.='<li';if($view=='proofs'){$content.=' class="libr8 active"';}$content.='><a href="admin/proofs">Proofs</a></li>';
         }
-		$content.='<li';if($view=='messages'){$content.=' class="active"';}$content.='><a href="admin/messages">Messages</a></li>';
+			$content.='<li';if($view=='messages'){$content.=' class="libr8 active"';}$content.='><a href="admin/messages">Messages</a></li>';
         if($user['options']{4}==1||$user['options']{1}==1){
-			$content.='<li';if($view=='preferences'){$content.=' class="active"';}$content.='><a href="admin/preferences">Preferences</a></li>';
+			$content.='<li';if($view=='preferences'){$content.=' class="libr8 active"';}$content.='><a href="admin/preferences">Preferences</a></li>';
         }
 	   if($user['options']{0}==1){
-			$content.='<li><button class="btn btn-success dropdown-toggle" style="margin:7px 10px;" data-toggle="dropdown" data-placement="right">Add <i class="caret"></i></button><ul class="dropdown-menu multi-level pull-right">';
+			$content.='<li>';
+				$content.='<button class="libr8 btn btn-success dropdown-toggle" style="margin:7px 10px;" data-toggle="dropdown" data-placement="right">Add <i class="libr8 caret"></i></button>';
+				$content.='<ul class="libr8 dropdown-menu multi-level pull-right">';
            $sq=$db->query("SELECT * FROM menu WHERE active='1' AND contentType!='index' AND contentType!='contactus' AND contentType!='cart' AND contentType!='tos' ORDER BY ord ASC");
            while($sr=$sq->fetch(PDO::FETCH_ASSOC)){
-               $content.='<li><a href="'.$sr['contentType'].'/add">'.$sr['title'].'</a></li>';
+					$content.='<li><a href="'.$sr['contentType'].'/add">'.$sr['title'].'</a></li>';
             }
-           $content.='<li><a href="admin/proofs/add">Proofs</a></li><li><a href="admin/accounts/add">Account</a></li><li class="dropdown-submenu pull-left"><a href="#">Orders</a><ul class="dropdown-menu"><li><a href="admin/orders/addquote">Quote</a></li><li><a href="admin/orders/addinvoice">Invoice</a></li></ul></li></ul></li>';
+					$content.='<li><a href="admin/proofs/add">Proofs</a></li>';
+					$content.='<li><a href="admin/accounts/add">Account</a></li>';
+					$content.='<li class="libr8 dropdown-submenu pull-left">';
+						$content.='<a href="#">Orders</a>';
+						$content.='<ul class="libr8 dropdown-menu">';
+							$content.='<li><a href="admin/orders/addquote">Quote</a></li>';
+							$content.='<li><a href="admin/orders/addinvoice">Invoice</a></li>';
+						$content.='</ul>';
+					$content.='</li>';
+				$content.='</ul>';
+			$content.='</li>';
         }
 	}else{
         $content.='<li';
-        if($view=='proofs')$content.=' class="active"';
+        if($view=='proofs')$content.=' class="libr8 active"';
         $content.='><a href="proofs">Proofs</a></li><li';
-        if($view=='orders')$content.=' class="active"';
+        if($view=='orders')$content.=' class="libr8 active"';
         $content.='><a href="orders">Orders</a></li><li';
-        if($view=='settings')$content.=' class="active"';
+        if($view=='settings')$content.=' class="libr8 active"';
         $content.='><a href="settings">Settings</a></li>';
     }
     $content.='</ul>';
@@ -45,5 +68,5 @@ if($user['rank']>0){
 	}else{
 		$avatar=$noavatar;
 	}
-    $content.='<img id="avatar" class="pull-right img-circle" src="'.$avatar.'"></div></div></nav></div>';
+    $content.='<img id="avatar" class="libr8 pull-right img-circle" src="'.$avatar.'"></div></div></nav></div>';
 }
