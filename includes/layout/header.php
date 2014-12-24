17b5b7154ca8fc5b94bd9de77d2d2f3943271534
StudioJunkyard <info@studiojunkyard.com>
2014-12-24 23:21:48 +1100

    Fix menu URL routing

--- includes/layout/header.php
+++ includes/layout/header.php
@@ -9,37 +9,37 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="<?php echo URL.'/admin';?>"><img src="includes/images/librecms.png"></a>
+				<a class="navbar-brand" href="<?php echo URL.'admin';?>"><img src="includes/images/librecms.png"></a>
 			</div>
 			<div class="collapse navbar-collapse" id="admin-collapse">
 				<ul class="nav navbar-nav pull-right relative">
-					<li<?php if($view=='statistics'){echo' class="active"';}?>><a href="admin/statistics">Statistics</a></li>
-					<li<?php if($view=='content'||$view=='article'||$view=='portfolio'||$view=='events'||$view=='news'||$view=='testimonials'||$view=='inventory'||$view=='services'||$view=='gallery'){echo' class="active"';}?>><a href="admin/content">Content</a></li>
-					<li<?php if($view=='bookings'){echo' class="active"';}?>><a href="admin/bookings">Bookings</a></li>
-					<li<?php if($view=='orders'){echo' class="active"';}?>><a href="admin/orders/all">Orders</a></li>
-					<li<?php if($view=='media'){echo' class="active"';}?>><a href="admin/media">Media</a></li>
-					<li<?php if($view=='accounts'){echo' class="active"';}?>><a href="admin/accounts">Accounts</a></li>
-					<li<?php if($view=='messages'){echo' class="active"';}?>><a href="admin/messages">Messages</a></li>
-					<li<?php if($view=='preferences'){echo' class="active"';}?>><a href="admin/preferences">Preferences</a></li>
+					<li<?php if($view=='statistics'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/statistics">Statistics</a></li>
+					<li<?php if($view=='content'||$view=='article'||$view=='portfolio'||$view=='events'||$view=='news'||$view=='testimonials'||$view=='inventory'||$view=='services'||$view=='gallery'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/content">Content</a></li>
+					<li<?php if($view=='bookings'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/bookings">Bookings</a></li>
+					<li<?php if($view=='orders'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/orders/all">Orders</a></li>
+					<li<?php if($view=='media'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/media">Media</a></li>
+					<li<?php if($view=='accounts'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/accounts">Accounts</a></li>
+					<li<?php if($view=='messages'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/messages">Messages</a></li>
+					<li<?php if($view=='preferences'){echo' class="active"';}?>><a href="<?php echo URL;?>admin/preferences">Preferences</a></li>
 					<li>
 						<button class="btn btn-success dropdown-toggle" style="margin:7px 10px;" data-toggle="dropdown" data-placement="right">Add <i class="caret"></i></button>
 						<ul class="dropdown-menu multi-level pull-right">
-							<li><a href="<?php echo URL;?>/admin/add/article">Article</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/portfolio">Portfolio</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/events">Event</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/news">News</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/testimonials">Testimonial</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/inventory">Inventory</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/services">Service</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/gallery">Gallery</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/bookings">Booking</a></li>
-							<li><a href="<?php echo URL;?>/admin/add/proofs">Proofs</a></li>
-							<li><a href="<?php echo URL;?>/admin/accounts/add">Account</a></li>
+							<li><a href="<?php echo URL;?>admin/add/article">Article</a></li>
+							<li><a href="<?php echo URL;?>admin/add/portfolio">Portfolio</a></li>
+							<li><a href="<?php echo URL;?>admin/add/events">Event</a></li>
+							<li><a href="<?php echo URL;?>admin/add/news">News</a></li>
+							<li><a href="<?php echo URL;?>admin/add/testimonials">Testimonial</a></li>
+							<li><a href="<?php echo URL;?>admin/add/inventory">Inventory</a></li>
+							<li><a href="<?php echo URL;?>admin/add/services">Service</a></li>
+							<li><a href="<?php echo URL;?>admin/add/gallery">Gallery</a></li>
+							<li><a href="<?php echo URL;?>admin/add/bookings">Booking</a></li>
+							<li><a href="<?php echo URL;?>admin/add/proofs">Proofs</a></li>
+							<li><a href="<?php echo URL;?>admin/accounts/add">Account</a></li>
 							<li class="dropdown-submenu pull-left">
 								<a href="#">Orders</a>
 								<ul class="dropdown-menu">
-									<li><a href="<?php echo URL;?>/admin/orders/addquote">Quote</a></li>
-									<li><a href="<?php echo URL;?>/admin/orders/addinvoice">Invoice</a></li>
+									<li><a href="<?php echo URL;?>admin/orders/addquote">Quote</a></li>
+									<li><a href="<?php echo URL;?>admin/orders/addinvoice">Invoice</a></li>
 								</ul>
 							</li>
 						</ul>
