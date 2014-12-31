300925de0e93bcd5e9e2fb87630e567a0ce8fca9
StudioJunkyard <info@studiojunkyard.com>
2015-01-01 01:21:21 +1100

    Updates before a Storm Hits

--- core/layout/login.php
+++ core/layout/login.php
@@ -14,35 +14,38 @@
 		<link rel="stylesheet" type="text/css" href="core/css/bootstrap.min.css" />
 		<link rel="stylesheet" type="text/css" href="core/css/admin.css" />
 	</head>
-	<body class="login">
+	<body>
 		<div class="container">
-			<div class="col-lg-4 col-md-4 col-sm-3"></div>
-			<main class="panel panel-default col-lg-4 col-md-5 col-sm-6 loginbox">
-				<div class="panel-body">
-					<form method="post" action="" accept-charset="UTF-8">
-						<input type="hidden" name="act" value="login">
-						<div class="form-group">
-							<label for="username" class="control-label col-lg-4 col-md-4 col-sm-4<?php if(isset($login)&&$login==false){echo' text-danger';}?>">Username</label>
-							<div class="input-group col-lg-8 col-md-8 col-sm-8<?php if(isset($login)&&$login==false){echo' has-error';}?>">
-								<input type="text" id="username" class="form-control" name="username" value="" placeholder="Enter a Username..." autofocus>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="password" class="control-label col-lg-4 col-md-4 col-sm-4<?php if(isset($login)&&$login==false){echo' text-danger';}?>">Password</label>
-							<div class="input-group col-lg-8 col-md-8 col-sm-8<?php if(isset($login)&&$login==false){echo' has-error';}?>">
-								<input type="password" id="password" class="form-control" name="password" placeholder="Enter a Password..." autocomplete="off">
-							</div>
-						</div>
-						<div class="form-group">
-							<label class="control-label col-lg-4 col-md-4 col-sm-4">&nbsp;</label>
-							<div class="input-group col-lg-8 col-md-8 col-sm-8">
-								<button class="btn btn-<?php if(isset($login)&&$login==false){echo'danger';}else{echo'success';}?> btn-large btn-block">Login</button>
-							</div>
-						</div>
-					</form>
+			<div class="col-md-4 col-md-offset-4">
+				<div class="login-panel panel panel-default">
+					<div class="panel-body">
+						<form role="form" method="post" action="" accept-charset="UTF-8">
+							<fieldset>
+								<legend>Login</legend>
+								<input type="hidden" name="act" value="login">
+								<div class="form-group">
+									<label for="username" class="control-label col-lg-4 col-md-4 col-sm-4<?php if(isset($login)&&$login==false){echo' text-danger';}?>">Username</label>
+									<div class="input-group col-lg-8 col-md-8 col-sm-8<?php if(isset($login)&&$login==false){echo' has-error';}?>">
+										<input type="text" id="username" class="form-control" name="username" value="" placeholder="Enter a Username..." autofocus>
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="password" class="control-label col-lg-4 col-md-4 col-sm-4<?php if(isset($login)&&$login==false){echo' text-danger';}?>">Password</label>
+									<div class="input-group col-lg-8 col-md-8 col-sm-8<?php if(isset($login)&&$login==false){echo' has-error';}?>">
+										<input type="password" id="password" class="form-control" name="password" placeholder="Enter a Password..." autocomplete="off">
+									</div>
+								</div>
+								<div class="form-group">
+									<label class="control-label col-lg-4 col-md-4 col-sm-4">&nbsp;</label>
+									<div class="input-group col-lg-8 col-md-8 col-sm-8">
+										<button class="btn btn-<?php if(isset($login)&&$login==false){echo'danger';}else{echo'success';}?> btn-large btn-block">Login</button>
+									</div>
+								</div>
+							</fieldset>
+						</form>
+					</div>
 				</div>
-			</main>
-			<div class="col-lg-4 col-md-4 col-sm-3"></div>
+			</div>
 		</div>
 		<footer class="clearfix navbar navbar-default navbar-fixed-bottom login">
 			<div class="logo"><img src="core/images/librecms.png" alt="LibreCMS"></div>
