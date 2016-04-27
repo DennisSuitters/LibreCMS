67df019f441b077b4a37f8f3d584c1f06ae983d6
StudioJunkyard <info@studiojunkyard.com>
2016-04-27 18:26:22 +1000

    Featured Content now reads from folder and database

--- core/view/featured.php
+++ core/view/featured.php
@@ -4,35 +4,69 @@ $html=preg_replace('~<settings.*?>~is','',$html,1);
 $itemCount=$matches[1];
 $limit=', '.$matches[1];
 if($itemCount==0){
-	$itemCount=5;
+	$itemCount=$config['showItems'];
 	$limit='';
 }
 $contentType=$matches[2];
 if($contentType=='all')$contentType='%';
-if($matches[3]=='asc'||$matches[3]=='ASC')
+if($matches[3]=='asc'||$matches[3]=='ASC'){
 	$order='ti ASC';
-elseif($matches[3]=='rand'||$matches[3]=='random')
+	$arrayOrder='asc';
+}elseif($matches[3]=='rand'||$matches[3]=='random'){
 	$order='RAND()';
-else
+	$arrayOrder='random';
+}else{
 	$order='ti DESC';
+	$arrayOrder='desc';
+}
 preg_match('/<items>([\w\W]*?)<\/items>/',$html,$matches);
 $it=$matches[1];
 $items='';
+$fi=0;
+$featuredfiles=array();
+if(file_exists('media/carousel/')){
+	foreach(glob('media/carousel/*.*') as $file){
+		if($file=='.')continue;
+		if($file=='..')continue;
+		$filetime=filemtime($file);
+		$featuredfiles[]=[
+			'contentType'=>'carousel',
+			'file'=>$file,
+			'title'=>'',
+			'link'=>'',
+			'ti'=>$filetime
+		];
+	}
+}
 $s=$db->prepare("SELECT * FROM content WHERE file!='' AND featured='1' AND internal!='1' AND status='published' AND contentType LIKE :contentType ORDER BY $order $limit");
 $s->execute(array(':contentType'=>$contentType));
+while($r=$s->fetch(PDO::FETCH_ASSOC)){
+	$filechk=basename($r['file']);
+	if(file_exists('media'.DS.$filechk)){
+		$featuredfiles[]=[
+			'contentType'=>$r['contentType'],
+			'file'=>$r['file'],
+			'title'=>$r['title'],
+			'link'=>$r['contentType'].DS.str_replace(' ','-',$r['title']),
+			'ti'=>$r['ti']
+		];
+	}
+}
 $indicators='';
 $indicator='';
 $featuredIndicators='';
-$ii=$s->rowCount();
+if($arrayOrder=='random')shuffle($featuredfiles);
+elseif($arrayOrder=='asc')asort($featuredfiles);
+else arsort($featuredfiles);
+$featuredfiles=array_slice($featuredfiles,0,$itemCount);
+$ii=count($featuredfiles);
 $i=0;
 if($ii>0){
 	if(stristr($html,'<indicators>')){
 		preg_match('/<indicators>([\w\W]*?)<\/indicators>/',$html,$matches);
 		$indicator=$matches[1];
 	}
-	while($r=$s->fetch(PDO::FETCH_ASSOC)){
-		$filechk=basename($r['file']);
-		if(!file_exists('media'.DS.$filechk))continue;
+	foreach($featuredfiles as $key => $r){
 		$item=$it;
 		$indicatorItem=$indicator;
 		if($i==0){
@@ -43,20 +77,10 @@ if($ii>0){
 			$indicatorItem=str_replace('<print active>','',$indicatorItem);
 		}
 		$indicatorItem=str_replace('<print indicatorCount>',$i,$indicatorItem);
-		if($r['options']{0}==1){
-			$item=str_replace('<cost>','',$item);
-			$item=str_replace('</cost>','',$item);
-			$item=str_replace('<print content="cost">',$r['cost'],$item);
-		}else
-			$item=preg_replace('~<cost>.*?<\/cost>~is','',$item,1);
-		if($r['caption']!='')
-			$item=str_replace('<print content="caption">',$r['caption'],$item);
-		$item=str_replace('<print link>',$r['contentType'].'/'.str_replace(' ','-',$r['title']),$item);
+		$item=str_replace('<print link>',$r['contentType'].DS.str_replace(' ','-',$r['title']),$item);
 		$item=str_replace('<print content="image">','<img src="'.$r['file'].'" class="img-responsive" alt="'.$r['title'].'">',$item);
-		$item=str_replace('<print content=schemaType>',$r['schemaType'],$item);
 		$item=str_replace('<print content=title>',$r['title'],$item);
 		$item=str_replace('<print content="title">',$r['title'],$item);
-		$item=str_replace('<print content="contentType">','<div>'.$r['contentType'].'</div>',$item);
 		$items.=$item;
 		$i++;
 		$indicators.=$indicatorItem;
