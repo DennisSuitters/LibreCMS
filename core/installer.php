9de10fc7fab161d8b60c67b3332ad810645b8ef3
root <info@studiojunkyard.com>
2017-01-14 18:10:37 +1100

    Fix Installer Typo

--- core/installer.php
+++ core/installer.php
@@ -36,7 +36,7 @@ if($_POST['emailtrap']==''){
 		$txt.='username = '.$dbusername.PHP_EOL;
 		$txt.='password = '.$dbpassword.PHP_EOL;
 		$txt.='[system]'.PHP_EOL;
-		$txt.='version = '.time();
+		$txt.='version = '.time().PHP_EOL;
 		$txt.='url = /'.ltrim($sysurl).PHP_EOL;
 		if($sysadmin=='')$txt.='admin = admin'.PHP_EOL;
 		else$txt.='admin = '.$sysadmin.PHP_EOL;
